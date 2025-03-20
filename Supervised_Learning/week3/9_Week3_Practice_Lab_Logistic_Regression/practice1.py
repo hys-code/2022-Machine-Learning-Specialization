d065aff5852219a4e2cd7588e3ee09e34e09aec3
hys-code <186195715+hys-code@users.noreply.github.com>
2025-03-20 08:11:14 +0000

    completed week3 final lab

--- Supervised_Learning/week3/9_Week3_Practice_Lab_Logistic_Regression/practice1.py
+++ Supervised_Learning/week3/9_Week3_Practice_Lab_Logistic_Regression/practice1.py
@@ -17,7 +17,7 @@ def compute_cost(X, y, w, b, lambda_= 0):
     s = sigmoid(z)
     
     cost = -y*np.log(s) - (1-y)*np.log(1-s)
-    reg_cost = (lambda_/2*m) * np.sum(w**2)
+    reg_cost = (lambda_/(2*m)) * np.sum(w**2)
     total_cost = (1/m)*np.sum(cost) + reg_cost
     
     return total_cost
@@ -177,6 +177,158 @@ w,b, J_history,_ = gradient_descent(X_train ,y_train, initial_w, initial_b,
                                    compute_cost, compute_gradient, alpha, iterations, 0)
 
 plot_decision_boundary(w, b, X_train, y_train)
+
+
+# ********** Evaluating logistic regression **********
+def predict(X, w, b):
+    m, n = X.shape
+    p = np.zeros(m)
+    
+    z = np.dot(X, w) + b
+    p = np.where(sigmoid(z) > 0.5, 1, 0)
+
+    """
+    for i in range(m):
+        z_wb = 0
+        for j in range(n):
+            z_wb += X[i, j] * w[j]
+        z_wb += b
+        f_wb = sigmoid(z_wb)
+        p[i] = 1 if f_wb >= 0.5 else 0
+    """
+    
+    return p
+
+np.random.seed(1) 
+tmp_w = np.random.randn(2)
+tmp_b = 0.3
+tmp_X = np.random.randn(4, 2) - 0.5
+
+tmp_p = predict(tmp_X, tmp_w, tmp_b)
+print(f'Output of predict: shape {tmp_p.shape}, value {tmp_p}')
+
+# UNIT TESTS        
+predict_test(predict)
+
+
+#Compute accuracy on our training set (Expect: 92.00)
+p = predict(X_train, w,b)
+print('Train Accuracy: %f'%(np.mean(p == y_train) * 100))
+
+
+
+# ********* Part II **********
+def compute_cost_reg(X, y, w, b, lambda_ = 1):
+    m, n = X.shape
+    cost_without_reg = compute_cost(X, y, w, b)
+    
+    reg_cost = 0.
+    
+    for j in range(n):
+        reg_cost += w[j]**2
+
+    total_cost = cost_without_reg + (lambda_/(2*m)) * reg_cost
+    return total_cost
+
+
+plt.clf() 
+plt.cla()
+plt.close('all')
+
+X_train, y_train = load_data("data/ex2data2.txt")
+
+# print X_train
+print("X_train:", X_train[:5])
+print("Type of X_train:",type(X_train))
+
+# print y_train
+print("y_train:", y_train[:5])
+print("Type of y_train:",type(y_train))
+
+print ('The shape of X_train is: ' + str(X_train.shape))
+print ('The shape of y_train is: ' + str(y_train.shape))
+print ('We have m = %d training examples' % (len(y_train)))
+
+# Plot examples
+plot_data(X_train, y_train[:], pos_label="Accepted", neg_label="Rejected")
+
+# Set the y-axis label
+plt.ylabel('Microchip Test 2') 
+# Set the x-axis label
+plt.xlabel('Microchip Test 1') 
+plt.legend(loc="upper right")
+
+
+# perform feature mapping to creat a 27-dimentional vector
+# repeat multiplying (x1 x2) by (x1 x2)
+print("Original shape of data:", X_train.shape)
+
+mapped_X =  map_feature(X_train[:, 0], X_train[:, 1])
+print("Shape after feature mapping:", mapped_X.shape)
+
+print("X_train[0]:", X_train[0])
+print("mapped X_train[0]:", mapped_X[0])
+
+X_mapped = map_feature(X_train[:, 0], X_train[:, 1])
+np.random.seed(1)
+initial_w = np.random.rand(X_mapped.shape[1]) - 0.5
+initial_b = 0.5
+lambda_ = 0.5
+cost = compute_cost_reg(X_mapped, y_train, initial_w, initial_b, lambda_)
+
+print("Regularized cost :", cost)
+
+# UNIT TEST    
+compute_cost_reg_test(compute_cost_reg)
+
+
+# Gradient for regularized logistic regression
+def compute_gradient_reg(X, y, w, b, lambda_ = 1): 
+    m, n = X.shape
+    dj_db, dj_dw = compute_gradient(X, y, w, b)
+    
+    dj_dw += (lambda_/m)*w
+    
+    return dj_db, dj_dw
+
+
+X_mapped = map_feature(X_train[:, 0], X_train[:, 1])
+np.random.seed(1) 
+initial_w  = np.random.rand(X_mapped.shape[1]) - 0.5 
+initial_b = 0.5
+ 
+lambda_ = 0.5
+dj_db, dj_dw = compute_gradient_reg(X_mapped, y_train, initial_w, initial_b, lambda_)
+
+print(f"dj_db: {dj_db}", )
+print(f"First few elements of regularized dj_dw:\n {dj_dw[:4].tolist()}", )
+
+# UNIT TESTS    
+compute_gradient_reg_test(compute_gradient_reg)
+
+# ************* Learning parameters using gradient descent *************
+# Initialize fitting parameters
+np.random.seed(1)
+initial_w = np.random.rand(X_mapped.shape[1])-0.5
+initial_b = 1.
+
+# Set regularization parameter lambda_ to 1 (you can try varying this)
+lambda_ = 0.01;                                          
+# Some gradient descent settings
+iterations = 10000
+alpha = 0.01
+
+w,b, J_history,_ = gradient_descent(X_mapped, y_train, 
+                                    initial_w, initial_b, 
+                                    compute_cost_reg, compute_gradient_reg, 
+                                    alpha, iterations, lambda_)
+
+plot_decision_boundary(w, b, X_mapped, y_train)
+
+#Compute accuracy on the training set
+p = predict(X_mapped, w, b)
+
+print('Train Accuracy: %f'%(np.mean(p == y_train) * 100))
 
 plt.show(block=True)
 
